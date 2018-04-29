3802fc8e65a8700707d61f10ba66c99858b8929c
Prabin Joshi <prbnjoshi20@gmail.com>
2018-04-30 00:59:37 +0545

    Add files via upload

---
+++ ComputerFactory.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+/**
+ * 
+ */
+public class Computerfactory {
+	public static Computer getComputer(String type, String ram, String hdd, String cpu){
+		if("PC".equalsIgnoreCase(type)) return new PC(ram, hdd, cpu);
+		else if("Server".equalsIgnoreCase(type)) return new Server(ram, hdd, cpu);
+		
+		return null;
+	}
+}
+
+
